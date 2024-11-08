891818fb53bd912b2ba4996d86d18242cbb39e4b
Spoilz1 <90699794+Spoilz1@users.noreply.github.com>
2024-11-07 19:13:02 -0500

    Update collisionHandler.cpp

--- src/core/collisionHandler.cpp
+++ src/core/collisionHandler.cpp
@@ -13,8 +13,8 @@ bool Game::collisions() {
 
     // Check collision with self (snake body)
     for (int i = 2; i < snakeLength; ++i) {
-        if (std::abs(snakeBody[1].xPos - snakeBody[i].xPos) <= tolerance &&
-            std::abs(snakeBody[1].yPos - snakeBody[i].yPos) <= tolerance) {
+        if (std::abs(snakeBody[0].xPos - snakeBody[i].xPos) <= tolerance &&
+            std::abs(snakeBody[0].yPos - snakeBody[i].yPos) <= tolerance) {
             return true;  // Collision detected
         }
     }
